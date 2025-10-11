6a26a16a639d8c20fc8cca8cd5cfa11d421ccb25
Ibrahim Abdelghany <abdelghanyibrahim24@gmail.com>
2025-10-11 23:19:21 +0300

    update

--- admin/layout/js/backend.js
+++ admin/layout/js/backend.js
@@ -4,15 +4,15 @@ let inputs = document.querySelectorAll(".form-control");
 if (eyeIcon) {
   eyeIcon.addEventListener("click", (e) => {
     if (e.target.classList.contains("fa-eye")) {
-      e.target.classList.remove("fa-eye")
-      e.target.classList.add("fa-eye-slash")
-      e.target.style.color = "#757575"
-      inputs[1].type = "password"
+      e.target.classList.remove("fa-eye");
+      e.target.classList.add("fa-eye-slash");
+      e.target.style.color = "#757575";
+      inputs[1].type = "password";
     } else {
-      e.target.classList.add("fa-eye")
-      e.target.classList.remove("fa-eye-slash")
-      e.target.style.color = "#008dde"
-      inputs[1].type = "text"
+      e.target.classList.add("fa-eye");
+      e.target.classList.remove("fa-eye-slash");
+      e.target.style.color = "#008dde";
+      inputs[1].type = "text";
     }
   })
 }
@@ -20,13 +20,13 @@ if (eyeIcon) {
 if (inputs) {
   inputs.forEach((input) => {
     input.addEventListener("focus", (e) => {
-      e.target.placeholder = ""
+      e.target.placeholder = "";
     })
   })
 
   inputs.forEach((input) => {
     input.addEventListener("blur", (e) => {
-      e.target.placeholder = e.target.dataset.text
+      e.target.placeholder = e.target.dataset.text;
     })
   })
 }
